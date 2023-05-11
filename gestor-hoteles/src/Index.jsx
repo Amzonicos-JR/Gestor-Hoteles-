599a95bdbbff85b915b8a9e63bc1b40b6a56e5a2
agutierrez-2021188 <agutierrez-2021188>
2023-05-11 11:35:34 -0600

    Se agrego estructura para el frontend

---
+++ gestor-hoteles/src/Index.jsx
@@ -0,0 +1,47 @@
+import React, { useState, createContext, useEffect } from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { NotFound } from './pages/NotFound';
+import { HomePage } from './pages/HomePage/HomePage';
+import { LoginPage } from './pages/LoginPage';
+import App from './App'
+
+export const AuthContext = createContext();
+export const Index = () => {
+
+    const [loggedIn, setLoggedIn] = useState(false)
+
+    const [dataUser, setDataUser] = useState({
+        name: '',
+        username: '',
+        role: ''
+      })
+
+      const [isAdmin, setIsAdmin] = useState(true);
+      useEffect(() => {
+          let token = localStorage.getItem('token')
+          if (token) setLoggedIn(true)
+      }, [])
+
+    const routes = createBrowserRouter([
+        {
+            path: '/',
+            element: <App/>,
+            errorElement: <NotFound/>,
+            children: [
+                {
+                    path: '/',
+                    element: <HomePage />
+                },
+                {
+                    path: '/login',
+                    element: <LoginPage></LoginPage>
+                }
+            ]
+        }
+    ])
+    return (
+        <AuthContext.Provider value={{ loggedIn, setLoggedIn, dataUser, setDataUser }}>
+            <RouterProvider router={routes}></RouterProvider>
+        </AuthContext.Provider>
+    )
+}
